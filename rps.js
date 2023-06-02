da9ada08c4787244d161a8409f6fa3c9475fe4ca
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-06-01 19:46:16 -0700

    Add fully random computer AI

---
+++ rps.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  choice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
+
+
