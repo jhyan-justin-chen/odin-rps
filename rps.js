6f93a187539bfa163f221ae94172905b1accef67
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-06-16 23:00:20 -0700

    Add score and refactor event handler structure
    Moved major functionality into individual functions.

--- rps.js
+++ rps.js
@@ -10,7 +10,7 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerChoice, computerChoice) {
+function calcRound(playerChoice, computerChoice) {
   if (playerChoice === "rock") {
     if (computerChoice === "rock") {
       return "tie";
@@ -38,21 +38,57 @@ function playRound(playerChoice, computerChoice) {
   }
 }
 
+function updateScore(roundResult) {
+  if (roundResult == "win") {
+    playerScore++;
+  } else if (roundResult == "lose") {
+    computerScore++;
+  }
+
+  const playerGameUI = document.querySelector(".game__player__result");
+  const computerGameUI = document.querySelector(".game__computer__result");
+  playerGameUI.textContent = playerScore;
+  computerGameUI.textContent = computerScore;
+
+  if (playerScore >= 5 || computerScore >= 5) {
+    finishGame(playerScore, computerScore);
+  }
+}
+
+function showRound(playerChoice, computerChoice, roundResult) {
+  const playerRoundUI = document.querySelector(".round__player__result");
+  const computerRoundUI = document.querySelector(".round__computer__result");
+  const roundResultUI = document.querySelector(".round__winner");
+  playerRoundUI.textContent = playerChoice.toUpperCase();
+  computerRoundUI.textContent = computerChoice.toUpperCase();
+  roundResultUI.textContent = roundResult.toUpperCase();
+}
+
+function playRound(e) {
+  const playerChoice = e.target.textContent.toLowerCase();
+  const computerChoice = getComputerChoice();
+  const roundResult = calcRound(playerChoice, computerChoice);
+
+  updateScore(roundResult);
+
+  showRound(playerChoice, computerChoice, roundResult);
+}
+
+function finishGame(playerScore, computerScore) {
+  const winnerGameUI = document.querySelector(".game__winner");
+  if (playerScore > computerScore) {
+    winnerGameUI.textContent = "Player Wins";
+  } else {
+    winnerGameUI.textContent = "Computer Wins";
+  }
+}
+
+let playerScore = 0;
+let computerScore = 0;
 
 const inputButtons = document.querySelectorAll(".controls__input");
 inputButtons.forEach((button) => {
-  button.addEventListener("mousedown", (e) => {
-    const playerChoice = e.target.textContent.toLowerCase();
-    const computerChoice = getComputerChoice();
-    const roundResult = playRound(playerChoice, computerChoice);
-
-    const playerRoundUI = document.querySelector(".round__player__result");
-    const computerRoundUI = document.querySelector(".round__computer__result");
-    const roundResultUI = document.querySelector(".round__winner");
-    playerRoundUI.textContent = playerChoice.toUpperCase();
-    computerRoundUI.textContent = computerChoice.toUpperCase();
-    roundResultUI.textContent = roundResult.toUpperCase();
-  });
+  button.addEventListener("mousedown", playRound);
 });
 
 // function game() {
